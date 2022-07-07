cf2aab536163c886abfa95a6d328dab73582c8c8
Haynn Tang <thanatoscharun@gmail.com>
2022-07-06 20:20:14 -0600

    added starter formats

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -6,7 +6,7 @@ import csv
 import os
 
 # Add a variable to load a file from a path.
-file_to_load = os.path.join("..", "Resources", "election_results.csv")
+file_to_load = os.path.join("Resources", "election_results.csv")
 # Add a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
@@ -15,10 +15,13 @@ total_votes = 0
 
 # Candidate Options and candidate votes.
 candidate_options = []
+
 candidate_votes = {}
 
 # 1: Create a county list and county votes dictionary.
 
+county_options=[]
+county_votes={}
 
 
 # Track the winning candidate, vote count and percentage
@@ -27,8 +30,9 @@ winning_count = 0
 winning_percentage = 0
 
 # 2: Track the largest county and county voter turnout.
-
-
+largest_county = ""
+wcounty_count = 0
+wcounty_percentage = 0
 
 # Read the csv and convert it into a list of dictionaries
 with open(file_to_load) as election_data:
